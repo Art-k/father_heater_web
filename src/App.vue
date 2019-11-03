a8cb9ddb0c7091041cf659de1a858c1c5fb7767f
kryhin <artem@maxtvmedia.com>
2019-11-03 21:06:42 +0200

    Board Tables

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,79 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container fluid>
+      <b-table
+              striped
+              hover
+              :items="boards.Entity"
+              :fields="fields"
+      >
+
+        <template v-slot:cell(added)="row">
+          {{ timestamp_to_datetime(row.item.added) }}
+        </template>
+
+        <template v-slot:cell(actions)="row">
+          <b-button block variant="success" @click="row.toggleDetails">
+            {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+          </b-button>
+        </template>
+
+        <template v-slot:row-details="row">
+          <b-card>
+            <SensorBoard :board="row.item.board"/>
+          </b-card>
+        </template>
+
+      </b-table>
+
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios'
+    import BootstrapVue from 'bootstrap-vue'
+    import { ButtonGroupPlugin } from 'bootstrap-vue'
+
+    import 'bootstrap/dist/css/bootstrap.css'
+    import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+    import Vue from 'vue'
+    Vue.use(BootstrapVue);
+    Vue.use(ButtonGroupPlugin);
+
+    import SensorBoard from './components/SensorBoard.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    SensorBoard
+  },data () {
+        return {
+            fields : [
+                { key: 'id', label: '#', sortable: true, sortDirection: 'desc' },
+                { key: 'board', label: 'MAC', sortable: true },
+                { key: 'name', label: 'Name', sortable: true },
+                { key: 'description', label: 'Description', sortable: true },
+                { key: 'added', label: 'Added At', sortable: true },
+                { key: 'actions', label: 'Actions' }
+            ],
+            boards: {"Entity" : [{"board":"1 sgwrvwrv", "name":"1 wvsetbsrtb", "description":"1 sdfbvdryndtrny"}, {"board":"2 sgwrvwrv", "name":"2 wvsetbsrtb", "description":"2 sdfbvdryndtrny"}]},
+        }
+    },
+    mounted () {
+        axios
+            .get('http://ahome.tech/get_board')
+            .then(response => (this.boards = response.data))
+    },
+    methods:{
+      timestamp_to_datetime: function (timestamp){
+          let DateObj = new Date(timestamp*1000);
+          let result = "";
+          result = DateObj.getDate()+'.'+DateObj.getMonth()+'.'+DateObj.getFullYear()+' '+DateObj.getHours()+':'+DateObj.getMinutes()
+          return result
+      }
+    }
 }
 </script>
 
