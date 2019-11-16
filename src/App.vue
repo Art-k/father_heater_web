ab4ea920da4bac72e043c6372d192f634faf9b89
kryhin <artem@maxtvmedia.com>
2019-11-16 13:04:34 +0200

    prod env is added

--- src/App.vue
+++ src/App.vue
@@ -131,7 +131,7 @@ export default {
 		this.get_boards();
 
 		axios
-			.get('http://'+process.env.VUE_APP_HOST + process.env.VUE_APP_PORT1+VUE_APP_SENSOR_TYPES_END_POINT)
+			.get('http://'+process.env.VUE_APP_HOST + process.env.VUE_APP_PORT1 + process.env.VUE_APP_SENSOR_TYPES_END_POINT)
 			.then(response => (this.dataTypeOptions = response.data.entity))
 		},
 	methods:{
